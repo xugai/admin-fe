c802dc58c70c0c96738205a212102790d0ae8d69
Xugai <xuqiubing@yeah.net>
2019-01-28 23:36:43 +0800

    dev env initialize

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+/*
+* @Author: Xugai
+* @Date:   2019-01-27 21:02:28
+* @Last Modified by:   Xugai
+* @Last Modified time: 2019-01-27 23:22:01
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import 'font-awesome/css/font-awesome.min.css';
+
+
+ReactDOM.render(
+	<div>
+		<i className="fa fa-handshake-o"></i>
+		<h1>Hello,World!</h1>,
+	</div>,
+	document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+/*
+* @Author: Xugai
+* @Date:   2019-01-27 21:04:38
+* @Last Modified by:   Xugai
+* @Last Modified time: 2019-01-28 23:21:02
+*/
+// 推测require()是Node.js的库函数(类似JRE,它也会提供库函数供我们使用)
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/app.jsx',
+  	output: {
+    	path: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist/',
+    	filename: 'js/app.js'
+  	},
+  	module: {
+  		rules: [
+        // react语法(jsx)的解析
+    		{
+    			test: /\.m?jsx$/,
+      		exclude: /(node_modules)/,
+      		use: {
+        		loader: 'babel-loader',
+        		options: {
+          		presets: ['env', 'react']
+        		}
+      		}
+    		},
+        // css样式的解析
+    		{
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: "css-loader"
+          })
+        },
+        // sass样式的解析
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
+        },
+        // 图片文件的解析
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+                name: 'resource/[name].[ext]'
+              }
+            }
+          ]
+        },
+        // 图标字体的解析
+        {
+          test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+                name: 'resource/[name].[ext]'
+              }
+            }
+          ]
+        }
+  	 ],
+	},
+  plugins: [
+      // 处理html文件
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      // 独立css文件
+      new ExtractTextPlugin("css/[name].css"),
+      // 提取出公共模块
+      new webpack.optimize.CommonsChunkPlugin({
+          name: 'common',
+          filename: 'js/base.js'
+      })
+  ],
+  devServer: {
+    // contentBase: './dist'
+    port: 8081
+  }
+};
